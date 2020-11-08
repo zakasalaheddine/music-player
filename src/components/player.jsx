48d168c63aeaecda389f74cce5097337c922efd2
Zaka Salah Eddine <zakasalaheddine@gmail.com>
2020-11-08 17:58:29 +0100

    feat: update pause and play icons

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,11 +1,11 @@
-import { faAngleLeft, faAngleRight, faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faAngleLeft, faAngleRight, faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRef, useState } from "react";
 
 export default function Player({ song, isPlaying, setIsPlaying }) {
   const { audio } = song
   const audioRef = useRef(null)
-  const [songInfo, setSongInfo] = useState({ currentTime: null, duration: null })
+  const [songInfo, setSongInfo] = useState({ currentTime: 0, duration: 0 })
   const playSongHandler = () => {
     const { current } = audioRef
     isPlaying ? current.pause() : current.play()
@@ -33,7 +33,7 @@ export default function Player({ song, isPlaying, setIsPlaying }) {
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
-        <FontAwesomeIcon onClick={playSongHandler} className="play" icon={faPlay} size="2x" />
+        <FontAwesomeIcon onClick={playSongHandler} className="play" icon={isPlaying ? faPause : faPlay} size="2x" />
         <FontAwesomeIcon className="skip-forward" icon={faAngleRight} size="2x" />
       </div>
       <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} src={audio} ref={audioRef} />
